5e13cd38a653fa7a2fee39cf1d1712d5b77e7f3d
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2023-01-27 16:06:20 -0300

    chore: requisito 2

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 const UserSchema = (sequelize, DataTypes) => {
-  const UserTable = sequelize.define('users', {
-    display_name: DataTypes.STRING,
+  const UserTable = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
